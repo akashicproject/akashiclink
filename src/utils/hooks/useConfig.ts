d4c869b95c27edea0aa5d03a0803f26df1012cdd
developer <developer@akashiclink.com>
2025-04-03 22:52:00 +0100

    feat: add app version api

---
+++ src/utils/hooks/useConfig.ts
@@ -0,0 +1,32 @@
+import type { IConfigResponse } from '@helium-pay/backend';
+import { isPlatform } from '@ionic/react';
+import useSWR from 'swr';
+
+import fetcher from '../ownerFetcher';
+
+export const useConfig = () => {
+  const { data, error, mutate } = useSWR(`/config`, fetcher, {});
+  const config = (data || {}) as IConfigResponse;
+  return {
+    config: isPlatform('ios')
+      ? {
+          awMinVersion: config.awMinVersionIos,
+          awLatestVersion: config.awLatestVersionIos,
+          awUrl: config.awUrlIos,
+        }
+      : isPlatform('android')
+      ? {
+          awMinVersion: config.awMinVersionAndroid,
+          awLatestVersion: config.awLatestVersionAndroid,
+          awUrl: config.awUrlAndroid,
+        }
+      : {
+          awMinVersion: config.awMinVersionExtension,
+          awLatestVersion: config.awLatestVersionExtension,
+          awUrl: config.awUrlExtension,
+        },
+    isLoading: !error && !data,
+    isError: error,
+    mutate,
+  };
+};
