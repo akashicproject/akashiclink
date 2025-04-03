e9907f18f78d75c7580c2af8c72fcbe59d1f3dff
developer <developer@akashiclink.com>
2025-04-03 22:56:27 +0100

    chore: rename Akashic Pay to AkashicPay

--- storybook/customTheme.js
+++ storybook/customTheme.js
@@ -2,7 +2,7 @@ import { create } from '@storybook/theming/create';
 
 export default create({
   base: 'light',
-  brandTitle: 'Akashic Pay Storybook',
+  brandTitle: 'AkashicPay Storybook',
   brandImage: '/shared-assets/images/PayLogo.svg',
   brandTarget: '_self',
 });
