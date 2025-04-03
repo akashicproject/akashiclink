6a9d006c428ec0395f2d41491d8ed9035d005051
developer <developer@akashiclink.com>
2025-04-03 22:57:32 +0100

    fix: backend to accept custom header

--- src/utils/axios-helper.ts
+++ src/utils/axios-helper.ts
@@ -1,33 +1,33 @@
-// import { App } from '@capacitor/app';
-// import { Capacitor } from '@capacitor/core';
+import { App } from '@capacitor/app';
+import { Capacitor } from '@capacitor/core';
 import axios from 'axios';
 
-// import { getManifestJson } from './hooks/useCurrentAppInfo';
+import { getManifestJson } from './hooks/useCurrentAppInfo';
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    // 'Ap-Client': Capacitor.getPlatform(),
+    'Ap-Client': Capacitor.getPlatform(),
   },
   withCredentials: true,
 });
 
-// instance.interceptors.request.use(async (config) => {
-//   try {
-//     const appInfo = await App.getInfo();
-//     if (appInfo) {
-//       config.headers['Ap-Version'] = appInfo.version;
-//     }
-//   } catch (e) {
-//     console.warn(e);
-//
-//     // App.getInfo() does not work on web. Try manifest
-//     const manifestData = await getManifestJson();
-//     config.headers['Ap-Version'] = manifestData.version;
-//   }
-//
-//   return config;
-// });
+instance.interceptors.request.use(async (config) => {
+  try {
+    const appInfo = await App.getInfo();
+    if (appInfo) {
+      config.headers['Ap-Version'] = appInfo.version;
+    }
+  } catch (e) {
+    console.warn(e);
+
+    // App.getInfo() does not work on web. Try manifest
+    const manifestData = await getManifestJson();
+    config.headers['Ap-Version'] = manifestData.version;
+  }
+
+  return config;
+});
 
 export const axiosBase = instance;
