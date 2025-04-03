5f50f94473c04b4de23cb7baf45a40a0c1bdfe2b
developer <developer@akashiclink.com>
2025-04-03 22:56:40 +0100

    chore: update CryptoTransfer contract

--- src/utils/nitr0gen/nitr0gen-api.ts
+++ src/utils/nitr0gen/nitr0gen-api.ts
@@ -46,7 +46,7 @@ enum ProductionContracts {
 enum TestNetContracts {
   Namespace = 'akashic',
   Create = 'c4f1186c58f49db2fdba401a1b36832902325d11a2e69ac6ef800836274c6894@3.1.0',
-  CryptoTransfer = 'd1903e29ea83413ecc759d129f7a21e4f8039ac5650360cf83d993343b5ffaa6@3.1.0',
+  CryptoTransfer = 'd1903e29ea83413ecc759d129f7a21e4f8039ac5650360cf83d993343b5ffaa6@5.1.13',
   DiffConsensus = '76869d5f632c283324b0cb7c8e16ba14eec2cf5d6d7b3f4521cc9b6a12818623@3.0.1',
   Onboard = 'b089a212ac22f57e2bef7d8a7f25702ebda98173939be2eba1ac0c2523d77383@5.0.2',
   NFTNamespace = 'candypig',
