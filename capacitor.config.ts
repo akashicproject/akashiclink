ddbeb353ce18a56a2f5617859c8b1255fe8d4087
developer <developer@akashiclink.com>
2025-04-03 22:52:18 +0100

    fix(ios/android): live reloading fixed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -19,7 +19,7 @@ const config: CapacitorConfig = {
     },
   },
   server:
-    process.env.IS_LIVE_RELOAD === 'true'
+    process.env.NODE_ENV === 'dev'
       ? {
           url: 'http://127.0.0.1:8100',
           cleartext: true,
