3a43312ec7fa573673b7dbeead29da6da7debce1
developer <developer@akashiclink.com>
2025-04-03 22:56:40 +0100

    chore: skip L2 verify

--- src/utils/hooks/useVerifyTxnAndSign.ts
+++ src/utils/hooks/useVerifyTxnAndSign.ts
@@ -1,31 +1,67 @@
 import { datadogRum } from '@datadog/browser-rum';
-import { L2Regex } from '@helium-pay/backend';
+import {
+  type ITransactionVerifyResponse,
+  L2Regex,
+  TransactionLayer,
+} from '@helium-pay/backend';
 
 import type { ValidatedAddressPair } from '../../components/send-deposit/send-form/types';
 import { useAppSelector } from '../../redux/app/hooks';
 import { selectCacheOtk } from '../../redux/slices/accountSlice';
 import { selectFocusCurrencyDetail } from '../../redux/slices/preferenceSlice';
 import { OwnersAPI } from '../api';
-import { signTxBody } from '../nitr0gen/nitr0gen-api';
+import type { L2TxDetail } from '../nitr0gen/nitr0gen.interface';
+import { Nitr0genApi, signTxBody } from '../nitr0gen/nitr0gen-api';
 import { unpackRequestErrorMessage } from '../unpack-request-error-message';
+import { useAccountStorage } from './useLocalAccounts';
 
 export const useVerifyTxnAndSign = () => {
   const { chain, token } = useAppSelector(selectFocusCurrencyDetail);
   const cacheOtk = useAppSelector(selectCacheOtk);
+  const { activeAccount } = useAccountStorage();
 
   return async (validatedAddressPair: ValidatedAddressPair, amount: string) => {
     const isL2 = L2Regex.exec(validatedAddressPair?.convertedToAddress);
+    const nitr0genApi = new Nitr0genApi();
 
     try {
-      if (!cacheOtk) {
+      if (!cacheOtk || !activeAccount) {
         return 'GenericFailureMsg';
       }
-      const txns = await OwnersAPI.verifyTransactionUsingClientSideOtk({
-        toAddress: validatedAddressPair.convertedToAddress,
-        amount,
-        coinSymbol: chain,
-        tokenSymbol: token,
-      });
+
+      let txns: ITransactionVerifyResponse[];
+      try {
+        txns = await OwnersAPI.verifyTransactionUsingClientSideOtk({
+          toAddress: validatedAddressPair.convertedToAddress,
+          amount,
+          coinSymbol: chain,
+          tokenSymbol: token,
+        });
+      } catch (error) {
+        datadogRum.addError(error);
+
+        // fallback to build transaction locally
+        if (isL2) {
+          const l2TxDetail: L2TxDetail = {
+            toAddress: validatedAddressPair.convertedToAddress,
+            amount,
+            coinSymbol: chain,
+            tokenSymbol: token,
+          };
+          const txBody = await nitr0genApi.L2Transaction(cacheOtk, l2TxDetail);
+          txns = [
+            {
+              ...l2TxDetail,
+              layer: TransactionLayer.L2,
+              fromAddress: activeAccount.identity,
+              txToSign: txBody,
+            },
+          ];
+        } else {
+          // TODO: build L1 transaction locally
+          return 'GenericFailureMsg';
+        }
+      }
 
       // reject the request if /verify returns multiple transfers
       // for L2: multiple transactions from the same Nitr0gen identity can always be combined into a single one
