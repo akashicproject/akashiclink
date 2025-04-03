e15e5caa633ddd903e27d65030f3611c3a68e74b
developer <developer@akashiclink.com>
2025-04-03 22:56:50 +0100

    refactor: wallet's `convertToFromASPrefix` to two functions

--- src/utils/convert-as-prefix.ts
+++ src/utils/convert-as-prefix.ts
@@ -1,25 +1,58 @@
-import { L2Regex } from '@helium-pay/backend';
-
 type ConversionDirection = 'to' | 'from';
 
-const L2RegexWithoutPrefix = /^[A-Fa-f\d]{64}$/;
+const L2RegexWithOptionalPrefix = /^(AS)?[A-Fa-f\d]{64}$/;
 
+/**
+ * Ensures a UMID/L2-address has, or does not have, exactly one AS prefix.
+ * i.e. it is idempotent.
+ * @throws Error if the umid argument isn't a valid UMID/L2-address with or
+ * without the prefix
+ * @deprecated use {@link prefixWithAS} or {@link removeASPrefix} instead
+ */
 export function convertToFromASPrefix(
   umid: string,
   direction: ConversionDirection
 ): string {
+  const result = L2RegexWithOptionalPrefix.exec(umid);
+  if (!result)
+    throw new Error(`${umid} does not match regex with or without prefix`);
+
+  const hasPrefix = result[1];
   if (direction === 'to') {
-    if (L2Regex.exec(umid)) {
-      return umid;
-    } else if (L2RegexWithoutPrefix.exec(umid)) {
-      return 'AS' + umid;
-    }
+    if (hasPrefix) return umid;
+    return `AS${umid}`;
   } else {
-    if (L2Regex.exec(umid)) {
-      return umid.slice(2);
-    } else if (L2RegexWithoutPrefix.exec(umid)) {
-      return umid;
-    }
+    if (hasPrefix) return umid.slice(2);
+    return umid;
   }
-  throw new Error(`${umid} does not match regex with or without prefix`);
+}
+
+/**
+ * Ensures a UMID/L2-address has exactly one AS prefix. i.e. it is idempotent.
+ * @throws Error if the umid argument isn't a valid UMID/L2-address with or
+ * without the prefix
+ */
+export function prefixWithAS<Umid extends string>(umid: Umid): `AS${Umid}` {
+  const result = L2RegexWithOptionalPrefix.exec(umid);
+  if (!result)
+    throw new Error(`${umid} does not match regex with or without prefix`);
+
+  if (result[1]) return umid as `AS${Umid}`;
+
+  return `AS${umid}`;
+}
+
+/**
+ * Ensures a UMID/L2-address doesn't have a AS prefix. i.e. it is idempotent.
+ * @throws Error if the umid argument isn't a valid UMID/L2-address with or
+ * without the prefix
+ */
+export function removeASPrefix(umid: string) {
+  const result = L2RegexWithOptionalPrefix.exec(umid);
+  if (!result)
+    throw new Error(`${umid} does not match regex with or without prefix`);
+
+  if (result[1]) return umid.slice(2);
+
+  return umid;
 }
