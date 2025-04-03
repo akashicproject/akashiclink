1eb23bb4ef271afd600e467fb65ae622fd7195c1
developer <developer@akashiclink.com>
2025-04-03 22:56:25 +0100

    feat: add nitr0gen testnet contracts

--- src/utils/nitr0gen-api.ts
+++ src/utils/nitr0gen-api.ts
@@ -2,10 +2,18 @@ import type { IBaseTransaction } from '@activeledger/sdk';
 import { TransactionHandler } from '@activeledger/sdk';
 import type { IKeyExtended } from '@activeledger/sdk-bip39';
 
-enum Nitr0gen {
+enum ProductionContracts {
   Namespace = 'notabox.keys',
   Onboard = 'df9e4e242c58cc6a03ca1679f007c7a04cad72c97fdb74bdfe9a4e1688077a79@1.4.0',
 }
+
+enum TestNetContracts {
+  Namespace = 'akashic',
+  Onboard = 'b089a212ac22f57e2bef7d8a7f25702ebda98173939be2eba1ac0c2523d77383@1.5.0',
+}
+
+const Nitr0gen =
+  process.env.NODE_ENV === 'prod' ? ProductionContracts : TestNetContracts;
 
 export async function signTxBody<T extends IBaseTransaction>(
   txBody: T,
